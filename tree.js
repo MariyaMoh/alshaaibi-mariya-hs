9247ec36435bf4061d645a374bf95b280792e0b8
MariyaMoh <alshaibi199732@gmail.com>
2022-06-20 20:46:40 +0200

    homework 2

--- tree.js
+++ tree.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 
-const recurse = (file) => {
+const recurse = (file, depth = 0) => {
   const stats = fs.statSync(file);
 
   if (stats.isDirectory()) {
-    console.log(file + " dir");
-    fs.readdirSync(file).forEach(child => recurse(`${file}/${child}`));
+    console.log(" ".repeat(depth) + " -> " + file + " dir");
+    fs.readdirSync(file).forEach(child => recurse(`${file}/${child}`, depth + 1));
   } else {
-    console.log(file + " file");
+    console.log(" ".repeat(depth) + " -- " + file + " file");
   }
 }
 
