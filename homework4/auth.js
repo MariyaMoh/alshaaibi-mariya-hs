f4b5e2b1e81519c3b2bb7b1cca84fab41842adde
MariyaMoh <alshaibi199732@gmail.com>
2022-06-27 11:17:51 +0200

    commit2

--- homework4/auth.js
+++ homework4/auth.js
@@ -1,17 +1,25 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
+const GOOGLE_CLIENT_ID =
+  '507577678887-va98ng5epsrvft3vjhn7pk1bqekr87gh.apps.googleusercontent.com';
+const GOOGLE_CLIENT_SECRET = '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';
 passport.use(
   new GoogleStrategy(
     {
       clientID: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
-      callbackURL: 'http://www.example.com/auth/google/callback',
+      callbackURL: 'http://localhost:3000/google/callback',
     },
     function (accessToken, refreshToken, profile, cb) {
-      User.findOrCreate({ googleId: profile.id }, function (err, user) {
-        return cb(err, user);
-      });
+      return cb(null, [profile]);
     }
   )
 );
+
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
--- homework4/index.js
+++ homework4/index.js
@@ -1,12 +1,39 @@
 const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+require('./auth');
+
+function isLoggden(req, res, next) {
+  req.user ? next() : res.sendStatus(401);
+}
 const app = express();
+app.use(session({ secret: 'cats' }));
+app.use(passport.initialize());
+app.use(passport.session()); 
 
 //1st argument (path or url) 2nd call back function (insaid 2 arg)
 app.get('/', (req, res) => {
   res.send('<a href="/auth/google">Authentication with Google');
 });
 
-app.get('/protected', (req, res) => {
+app.get(
+  '/auth/google',
+  passport.authenticate('google', { scope: ['email', 'profile'] })
+);
+
+app.get(
+  '/google/callback',
+  passport.authenticate('google', {
+    successRedirect: '/protected',
+    failureRedirect: '/auth/failure',
+  })
+);
+
+app.get('/auth/failure', (req, res) => {
+  res.send('something went wrong!!');
+});
+
+app.get('/protected', isLoggden, (req, res) => {
   res.send('Hello From Mariya !!');
 });
 app.listen(3000, () => console.log('listening omn port 3000'));
